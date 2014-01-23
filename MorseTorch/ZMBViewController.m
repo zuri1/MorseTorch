b8a549f6e93c82c5ae4a86997d9b7f32a3feb96c
zuri1 <zuri.biringer@gmail.com>
2014-01-22 16:29:37 -0800

    Create dictionary lowercase latin to morse code

--- MorseTorch/ZMBViewController.m
+++ MorseTorch/ZMBViewController.m
@@ -24,6 +24,34 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.myTextField.delegate = self;
+    
+    NSDictionary *dict = @{@"a" : @"-.",
+                           @"b" : @"-...",
+                           @"c" : @"-.-.",
+                           @"d" : @"-..",
+                           @"e" : @".",
+                           @"f" : @"..-.",
+                           @"g" : @"--.",
+                           @"h" : @"....",
+                           @"i" : @"..",
+                           @"j" : @".---",
+                           @"k" : @"-.-",
+                           @"l" : @".-..",
+                           @"m" : @"--",
+                           @"n" : @"-.",
+                           @"o" : @"---",
+                           @"p" : @".--.",
+                           @"q" : @"--.-",
+                           @"r" : @".-.",
+                           @"s" : @"...",
+                           @"t" : @"-",
+                           @"u" : @"..-",
+                           @"v" : @"...-",
+                           @"w" : @".--",
+                           @"x" : @"-..-",
+                           @"y" : @"-.--",
+                           @"z" : @"--.."};
+    NSLog(@"%@", dict);
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
